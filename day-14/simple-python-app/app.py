12a7f596bc52cf73000004aa026e375413169340
Safia-Addow <122935768+Safia-Addow@users.noreply.github.com>
2025-09-13 21:27:18 +0100

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world this is my first codepipeline'
 
 if __name__ == '__main__':
     app.run()
